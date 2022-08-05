ce1cf85f5fe38babe7a8b82521f5c1652ad61ee6
M Wildan F <wildanfd11@gmail.com>
2022-08-06 02:14:09 +0700

    Menambahkan handler untuk mengambil buku berdasarkan id, still working on error on postman

--- src/handler.js
+++ src/handler.js
@@ -110,4 +110,27 @@ const getAllBooksHandler = (request, h) => {
   return response;
 };
 
-module.exports = { saveBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const book = books.filter((buku) => buku.id === bookId)[0];
+
+  if (book === undefined) {
+    const response = h.response({
+      status: "fail",
+      message: "Buku tidak ditemukan",
+    });
+    response.code(404);
+    return response;
+  }
+
+  const response = h.response({
+    status: "success",
+    data: {
+      book,
+    },
+  });
+  response.code(200);
+  return response;
+};
+module.exports = { saveBookHandler, getAllBooksHandler, getBookByIdHandler };
