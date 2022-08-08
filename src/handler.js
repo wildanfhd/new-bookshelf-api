b1af1df30f1a58177d01fcf4d9dbbbbccb5681cf
M Wildan F <wildanfd11@gmail.com>
2022-08-08 21:49:26 +0700

    Mengubah isi dari if statement

--- src/handler.js
+++ src/handler.js
@@ -118,22 +118,21 @@ const getBookByIdHandler = (request, h) => {
 
   const book = books.filter((buku) => buku.id === bookId)[0];
 
-  if (book !== undefined) {
+  if (!book) {
     const response = h.response({
-      status: "success",
-      data: {
-        book,
-      },
+      status: "fail",
+      message: "Buku tidak ditemukan",
     });
-    response.code(200);
+    response.code(404);
     return response;
   }
-
   const response = h.response({
-    status: "fail",
-    message: "Buku tidak ditemukan",
+    status: "success",
+    data: {
+      book,
+    },
   });
-  response.code(404);
+  response.code(200);
   return response;
 };
 module.exports = { saveBookHandler, getAllBooksHandler, getBookByIdHandler };
